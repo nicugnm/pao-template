0147773d75f66151ce8573a6b0e55f7d697c3895
nicugnm <nghergu17@gmail.com>
2023-03-22 12:43:40 +0200

    Template Project

---
+++ src/main/java/ro/pao/model/ExampleClass.java
@@ -0,0 +1,17 @@
+package ro.pao.model;
+
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+import ro.pao.model.abstracts.AbstractEntity;
+
+/**
+ * Exista @Builder si @SuperBuilder.
+ * @SuperBuilder da acces si la campurile la clasa din care se mosteneste.
+ * Concret, daca folosim @Builder, o sa avem acces doar la campurile din clasa ExampleClass
+ * Iar daca folosim @SuperBuilder o sa avem acces si la campurile din clasa ExampleClass, dar si din clasa AbstractEntity.
+ */
+@SuperBuilder
+@Getter
+public class ExampleClass extends AbstractEntity {
+
+}
